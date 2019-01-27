f52dc5766daf2751c82f66bee61d7a27b9aaab9c
kangmengmeng <37363608+kangmengmeng@users.noreply.github.com>
2019-01-27 22:06:25 +0800

    Update time.py

--- time.py
+++ time.py
@@ -1,3 +1,5 @@
+# crf as rnn中计算推理时间（CPU）
+# modify line 40 of https://github.com/sadeepj/crfasrnn_keras/blob/master/run_demo.py as shown below:
 
 probs = model.predict(img_data, verbose=False)[0, :, :, :]  # Warm-up
 probs = model.predict(img_data, verbose=False)[0, :, :, :]  # Warm-up
